7ae18d3d0a8e9a220115c9a6748f7a4ec074ab4b
Ruslan-Nikolaevich <ruslan_nic@mail.ru>
2021-08-29 18:27:04 +0300

    expression-calculator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let str = expr;
+    let rez,sum;
+    let countOpen = 0, countClose = 0;
+    let bracketOpen='(';
+    let bracketClose=')';
+   
+  //--------------------------------------------------Провека строки на скобки
+   function checkBracket(str) {
+     for (let i = 0; i < str.length; i++) {
+       bracketOpen === str[i] ? countOpen++ : false;
+       if (bracketClose === str[i]) {
+        countClose++;
+         if (countOpen - countClose < 0) {
+  
+           return false
+         }
+       }
+     }
+     if (countOpen - countClose !== 0) {
+      return false
+     }
+     return true
+   }
+  
+   let ch = checkBracket(str);
+  //  console.log(`Проверка на правильность скобок = ${ch}`);
+    if (ch === false) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    //   return (throw new Error('something went wrong'))
+    }
+      
+    function arifmeticsStr(str) {
+        str=String(str);
+        let err = str.indexOf('/ 0');
+        if (err !== -1) {
+          throw new Error("TypeError: Division by zero.");
+        } else {
+          subStr=eval(str);
+          return subStr;
+        } 
+      }
+    
+    
+   return arifmeticsStr(str);
 }
 
 module.exports = {
